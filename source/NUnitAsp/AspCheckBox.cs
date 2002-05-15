fe2634969473e6bcd30ca3d9a0fc58b97e3c6e00
jlittle82 <jlittle82>
2002-05-15 03:05:48 +0000

    Added check box tester.

--- source/NUnitAsp/AspCheckBox.cs
+++ source/NUnitAsp/AspCheckBox.cs
@@ -22,10 +22,30 @@ using System;
 
 namespace NUnit.Extensions.Asp
 {
-	public class AspRadioButton : AspCheckBox
+
+	public class AspCheckBox : AspControl
 	{
-		public AspRadioButton(string aspId, Control container) : base(aspId, container)
+		public AspCheckBox(string aspId, Control container) : base(aspId, container)
 		{
 		}
+
+		public bool Checked
+		{
+			get
+			{
+				return GetOptionalAttributeValue("checked") != "";
+			}
+			set
+			{
+				if (value == true) 
+				{
+					EnterInputValue(GetAttributeValue("name"), GetAttributeValue("value"));
+				}
+				else
+				{
+					throw new ApplicationException("not implemented");
+				}
+			}
+		}	
 	}
 }
