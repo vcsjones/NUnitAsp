0cc46ec9cc054668e69ea939c6e6e0fa5de4c0dd
lkhatskevitch <lkhatskevitch>
2004-07-30 14:58:58 +0000

    Addind unicode support test page

---
+++ source/NUnitAspTest/UnicodeTest.cs
@@ -0,0 +1,49 @@
+#region Copyright (c) 2004, Brian Knowles, Jim Shore
+/********************************************************************************************************************
+'
+' Copyright (c) 2004, Brian Knowles, Jim Shore
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+' the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+' to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+'
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+' of the Software.
+'
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+' THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+'
+'*******************************************************************************************************************/
+#endregion
+
+using System;
+using NUnit.Extensions.Asp.AspTester;
+using NUnit.Framework;
+
+namespace NUnit.Extensions.Asp.Test
+{
+	public class UnicodeTest : NUnitAspTestCase
+	{
+		protected override void SetUp()
+		{
+			base.SetUp();
+			Browser.GetPage(BaseUrl + "/UnicodeTestPage.aspx");
+		}
+
+		public void TestLabelText() 
+		{
+			LabelTester textLabel = new LabelTester("textLabel", CurrentWebForm);
+			AssertEquals("text", "foo", textLabel.Text);
+		}
+
+		public void TestCurrentPageText()
+		{
+			Assert("CurrentTextPage doesn't contain <html> tag",
+				Browser.CurrentPageText.IndexOf("<html>") != -1);
+		}
+	}
+}
